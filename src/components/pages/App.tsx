7bca4020db64f53f396ea01bab199c93c44800ec
vicTavares <victavares@hotmail.com>
2023-06-26 09:34:27 -0400

    feat(App.tsx): estados tarefas e selecionados

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Formulario from "../Formulario";
 import Lista from "../Lista";
-// import Formulario from "../components/Formulario";
-// import Lista from "../components/Lista";
-//import Cronometro from "../components/Cronometro";
 import style from "./App.module.scss";
 import Cronometro from "../Cronometro";
+import { ITarefa } from "../../types/tarefa";
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]); // to Type useState como uma array <ITarefa[] ou uma array vazia []
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
       <Cronometro />
     </div>
   );
