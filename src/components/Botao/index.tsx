650c79190a7f8934e8088973925b9222c197a39e
vicTavares <victavares@hotmail.com>
2023-06-26 09:56:36 -0400

    feat(Botao.tsx): altera propriedade do botao
    * Inclui os tipos   "button" | "submit" | "reset" para possibilitar chamadas desses tipo no form

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -6,13 +6,23 @@ import style from "./Botao.module.scss";
 
 //class Botao extends React.Component<{ texto: string }> {
 
-interface BotaoProps {
-  children: React.ReactNode;
-}
+//interface BotaoProps {
+//children: React.ReactNode;
+//}
 
-class Botao extends React.Component<BotaoProps> {
+//class Botao extends React.Component<BotaoProps> {
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset";
+  children?: React.ReactNode;
+}> {
+  // Necessrio adicionar no React.Component a propriedade type?: string para suportar opcionalmente a tipagem onde esse componente for utilizado
   render() {
-    return <button className={style.botao}>{this.props.children}</button>;
+    const { type = "button" } = this.props; // a propriedade padrao do botao e button podendo ser alterada opcionalmente pelo React.Component<{ type?: string }
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button> //  {this.props.type} {this.props.children}
+    );
   }
 }
 
