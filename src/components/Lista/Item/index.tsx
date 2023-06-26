2144a87468e046d4d89feacbea3af16926101510
vicTavares <victavares@hotmail.com>
2023-06-26 10:06:01 -0400

    build: inclui  uuid

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,17 +1,46 @@
+import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
-//function item(){
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
 export default function Item({
   tarefa,
   tempo,
-}: {
-  tarefa: string;
-  tempo: string;
-}) {
+  selecionado,
+  completado,
+  id,
+}: Props) {
+  console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
   return (
     <li className={style.item}>
       <h3> {tarefa} </h3>
       <span> {tempo} </span>
     </li>
   );
+}
+
+//function item(){
+
+{
+  /* return (
+    <li
+      className={style.item}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
+      <h3> {tarefa} </h3>
+      <span> {tempo} </span>
+    </li>
+  );
+}
+*/
 }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Lista.module.scss";
 import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
 
-function Lista() {
-  const tarefas = [
-    {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
-    },
-    {
-      id: "id",
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
-    },
-  ];
+interface Props {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+  //function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+/*function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+//function Lista(props: { tarefas: ITarefa[] }) {
+  // Lista transferida para App.tsx em pages
+
+  const [tarefas, setTarefas] = useState(props.tarefas);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2
+        onClick={() => {
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Estudar Estado", tempo: "05:00:00" },
+          ]);
+        }}
+      >
+        {" "}
+        Estudos do dia{" "}
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
-            //tarefa={item.tarefa} //importa intem individuamente
-            //tempo={item.tempo} //importa intem individuamente
+            //tarefa={item.tarefa} //import internal individually
+            //tempo={item.tempo} /import internal individually
             key={index}
             {...item} //importa todos os itens do arquivo de props item
           />
@@ -35,5 +54,6 @@ function Lista() {
     </aside>
   );
 }
+*/
 
 export default Lista;
