c09bc6cda817ebe4324fb0414b722f239af565d0
Estevão Costa <estevaocosta@sefaz.am.gov.br>
2023-06-29 08:33:17 -0400

    chore: comenta sobre próxima atividade

--- src/types/tarefa.ts
+++ src/types/tarefa.ts
@@ -1,7 +1,8 @@
 export interface ITarefa {
   tarefa: string;
   tempo: string;
+  id: string;
+  // TODO: trocar estes atributos para status: 'doing' | 'done' | 'todo'
   selecionado: boolean;
   completado: boolean;
-  id: string;
 }
