e040da38f2dbaf9f74e21840fd3df17c29dce6b7
vicTavares <victavares@hotmail.com>
2023-07-03 09:56:52 -0400

    :ok_hand:fix(tarefas.ts): Inclusão na interface ITarefa de status  com atributos específicos

--- src/types/tarefa.ts
+++ src/types/tarefa.ts
@@ -2,7 +2,6 @@ export interface ITarefa {
   tarefa: string;
   tempo: string;
   id: string;
+  status: "todo" | "doing" | "done" | "selected";
   // TODO: trocar estes atributos para status: 'doing' | 'done' | 'todo'
-  selecionado: boolean;
-  completado: boolean;
 }
