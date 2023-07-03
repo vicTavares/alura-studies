c2a34316fe089f11867ac9e839e832d6fd858e92
vicTavares <victavares@hotmail.com>
2023-07-03 09:15:10 -0400

    docs:books: Organizar código deleção de comentários

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,58 +1,22 @@
 import React from "react";
 import style from "./Botao.module.scss";
-import { Children } from "react";
-
-//import React, { Component } from "react";
-//import ReactDOM from "react-dom";
-
-//class Botao extends React.Component<{ texto: string }> {
-
-//interface BotaoProps {
-//children: React.ReactNode;
-//}
 
 interface Props {
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
   children?: React.ReactNode;
+  disabled?: boolean;
 }
 
-function Botao({ onClick, type, children }: Props) {
+export function Botao({ onClick, type, children, disabled }: Props) {
   return (
-    <button onClick={onClick} type={type} className={style.botao}>
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
 }
-
-//class Botao extends React.Component<BotaoProps> {
-class Botao1 extends React.Component<{
-  // type?: "button" | "submit" | "reset" | undefined;
-  type?: "button" | "submit" | "reset";
-  onClick?: () => void;
-  children?: React.ReactNode;
-}> {
-  // Necessrio adicionar no React.Component a propriedade type?: string para suportar opcionalmente a tipagem onde esse componente for utilizado
-  render() {
-    const { type = "button", onClick } = this.props; // a propriedade padrao do botao e button podendo ser alterada opcionalmente pelo React.Component<{ type?: string }
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.children}
-      </button> //  {this.props.type} {this.props.children}
-    );
-  }
-}
-
-/* class Botao extends React.Component {
-  render() {
-    return (
-      <button clasName={style.botao}>
-        {this.props.children}
-      </button>
-    )
-  }
-}*/
-
-// TODO: trocar export deafult por export nomeado neste e nos demais componentes
-
-export default Botao;
