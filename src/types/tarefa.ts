887559aed0168d276ae7c65ce5f0815ded995c70
vicTavares <victavares@hotmail.com>
2023-07-04 10:57:11 -0400

    fix: altera interface ITarefa - tempo para duracaoEmSegundos

--- src/types/tarefa.ts
+++ src/types/tarefa.ts
@@ -1,6 +1,6 @@
 export interface ITarefa {
   tarefa: string;
-  tempo: string;
+  duracaoEmSegundos: number;
   id: string;
   status: "todo" | "doing" | "done" | "selected";
   // TODO: trocar estes atributos para status: 'doing' | 'done' | 'todo'
