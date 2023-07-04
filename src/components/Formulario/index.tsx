1fc46ca4c5412adc93de26f6536a3886f8bd8fd6
vicTavares <victavares@hotmail.com>
2023-07-04 10:52:53 -0400

    fix (Formulario) : altera setTarefa relativo a tempo
    * altera de setDuracaoDaTarefa  do tipo time para Hora, Minuto, Segundo

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -10,7 +10,10 @@ interface Props {
 
 export function FormularioDeTarefa({ setTarefas }: Props) {
   const [textoDaTarefa, setTextoDaTarefa] = useState("");
-  const [duracaoDaTarefa, setDuracaoDaTarefa] = useState("00:00");
+  //const [duracaoDaTarefa, setDuracaoDaTarefa] = useState("00:00:00");
+  const [duracaoDaTarefaHora, setDuracaoDaTarefaHora] = useState(0);
+  const [duracaoDaTarefaMinuto, setDuracaoDaTarefaMinuto] = useState(0);
+  const [duracaoDaTarefaSegundo, setDuracaoDaTarefaSegundo] = useState(0);
 
   function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
@@ -18,20 +21,29 @@ export function FormularioDeTarefa({ setTarefas }: Props) {
       ...tarefasAntigas,
       {
         tarefa: textoDaTarefa,
-        tempo: duracaoDaTarefa,
+        //tempo: duracaoDaTarefa,
+        duracaoEmSegundos:
+          duracaoDaTarefaHora * 3600 +
+          duracaoDaTarefaMinuto * 60 +
+          duracaoDaTarefaSegundo,
         status: "todo",
         disabled: "true",
         id: uuidv4(),
       },
     ]);
     setTextoDaTarefa("");
-    setDuracaoDaTarefa("00:00");
+    // setDuracaoDaTarefa("00:00");
+    setDuracaoDaTarefaHora(0);
+    setDuracaoDaTarefaMinuto(0);
+    setDuracaoDaTarefaSegundo(0);
   }
 
   return (
     <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
       <div className={style.inputContainer}>
-        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <label htmlFor="tarefa" className={style.label}>
+          Adicione um novo estudo
+        </label>
         <input
           type="text"
           name="tarefa"
@@ -43,16 +55,47 @@ export function FormularioDeTarefa({ setTarefas }: Props) {
         />
       </div>
       <div className={style.inputContainer}>
-        <label htmlFor="tempo">Duração</label>
+        <span className={style.label}>Duração</span>
         <input
-          type="time"
+          type="number"
+          placeholder="horas"
           step="1"
-          name="duração"
-          value={duracaoDaTarefa}
-          onChange={(evento) => setDuracaoDaTarefa(evento.target.value)}
-          id="duração"
-          min="00:00:00"
-          max="01:30:00"
+          name="hora"
+          //value={duracaoDaTarefa}
+          value={duracaoDaTarefaHora}
+          onChange={(evento) =>
+            setDuracaoDaTarefaHora(Number(evento.target.value))
+          }
+          id="hora"
+          min="00"
+          required
+        />
+        <input
+          type="number"
+          placeholder="minutos"
+          step="1"
+          name="minuto"
+          //value={duracaoDaTarefa}
+          value={duracaoDaTarefaMinuto}
+          onChange={(evento) =>
+            setDuracaoDaTarefaMinuto(Number(evento.target.value))
+          }
+          id="minuto"
+          min="00"
+          required
+        />
+        <input
+          type="number"
+          placeholder="segundos"
+          step="1"
+          name="segundo"
+          //value={duracaoDaTarefa}
+          value={duracaoDaTarefaSegundo}
+          onChange={(evento) =>
+            setDuracaoDaTarefaSegundo(Number(evento.target.value))
+          }
+          id="segundo"
+          min="00"
           required
         />
       </div>
