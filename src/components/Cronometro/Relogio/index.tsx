bb9c40c41801f5d0d6ce531b835765112c232fd3
vicTavares <victavares@hotmail.com>
2023-07-06 09:39:15 -0400

    refactor: organização de código

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -3,12 +3,11 @@ import style from "./Relogio.module.scss";
 interface Props {
   tempo: number | undefined;
 }
+
 export default function Relogio({ tempo = 0 }: Props) {
   const minutos = Math.floor(tempo / 60);
   const segundos = tempo % 60;
-  //const minutosString = String(minutos)
   const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, "0"); // alterar o arquivo de configuracao tsconfig.json com "downlevelIteration": true,
-
   const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, "0"); //.padStart e a quantidade de caracteres minimo iniciais e qual o caractere que deve ser preenchido(2,'0')
   return (
     <>
